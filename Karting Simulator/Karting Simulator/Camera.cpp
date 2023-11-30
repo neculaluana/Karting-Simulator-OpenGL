62e3e3830189a30eccf844cecfecfdc12e36c720
Alex Popa <alexpopa1910@gmil.com>
2023-11-30 16:29:47 +0200

    Created Camera class and ProcessMouseMovement function

--- Karting Simulator/Karting Simulator/Camera.cpp
+++ Karting Simulator/Karting Simulator/Camera.cpp
@@ -1,1 +1,14 @@
 #include "Camera.h"
+
+void Camera::ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true)
+{
+	yaw += xOffset;
+	pitch += yOffset;
+	if (constrainPitch) {
+		if (pitch > 89.0f)
+			pitch = 89.0f;
+		if (pitch < -89.0f)
+			pitch = -89.0f;
+	}
+	//UpdateCameraVectors();
+}
--- Karting Simulator/Karting Simulator/Camera.h
+++ Karting Simulator/Karting Simulator/Camera.h
@@ -6,3 +6,20 @@
 	Up,
 	Down,
 };
+
+class Camera {
+private:
+	const float zNEAR = 0.1f;
+	const float zFAR = 500.f;
+	const float YAW = -90.0f;
+	const float PITCH = 0.0f;
+	const float FOV = 45.0f;
+	glm::vec3 startPosition;
+
+	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true);
+
+protected:
+
+public:
+
+};
