4dc5f7a8476823b6b5f3435fcb782364703bfaca
Deniisaa <iliedenisaa@gmail.com>
2023-11-30 15:46:53 +0200

    Created CreateShader function in Shader.cpp

--- Karting Simulator/Karting Simulator/Shader.cpp
+++ Karting Simulator/Karting Simulator/Shader.cpp
@@ -1,1 +1,22 @@
+#include "Shader.h"
+
+
+
+
+void Shader::CreateShaders(void)
+{
+    VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+    glCompileShader(VertexShaderId);
+
+    FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+    glCompileShader(FragmentShaderId);
+
+    ProgramId = glCreateProgram();
+    glAttachShader(ProgramId, VertexShaderId);
+    glAttachShader(ProgramId, FragmentShaderId);
+    glLinkProgram(ProgramId);
+    glUseProgram(ProgramId);
+}
 
