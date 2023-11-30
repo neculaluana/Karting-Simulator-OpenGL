23789b770196be73cf6bf09c023cbec656e98c63
Deniisaa <iliedenisaa@gmail.com>
2023-11-30 15:44:11 +0200

    Added Shader class and created VertexShader

---
+++ Karting Simulator/Karting Simulator/Shader.h
@@ -0,0 +1,30 @@
+#include <GL/glew.h>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<cerrno>
+
+
+
+class Shader {
+public:
+    void CreateShaders();
+    GLuint VertexShaderId;
+    GLuint ProgramId;
+    GLuint FragmentShaderId;
+    const GLchar* VertexShader =
+    {
+       "#version 400\n"\
+       "layout(location=0) in vec4 in_Position;\n"\
+       "layout(location=1) in vec4 in_Color;\n"\
+       "out vec4 ex_Color;\n"\
+       "void main(void)\n"\
+       "{\n"\
+       "  gl_Position = in_Position;\n"\
+       "  ex_Color = in_Color;\n"\
+       "}\n"
+    };
+
+
+};
