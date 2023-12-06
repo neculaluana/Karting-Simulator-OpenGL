8ad522b43da08b450e57bdbaa0c99247a49745a5
Necula Luana <neculaluana2003@gmail.com>
2023-12-06 20:26:52 +0200

    implemented Terrain constructor & destructor

--- Karting Simulator/Karting Simulator/Terrain.cpp
+++ Karting Simulator/Karting Simulator/Terrain.cpp
@@ -1,1 +1,14 @@
 #include "Terrain.h"
+Terrain::Terrain(int width, int height, const std::string& heightmapPath, float scale)
+    : width(width), height(height), scale(scale)
+{
+    terrainShader = new Shader("terrainVertexShader.vs", "terrainFragmentShader.fs");
+    loadHeightmap(heightmapPath);
+    generateTerrain();
+    setupMesh();
+}
+
+Terrain::~Terrain()
+{
+    delete terrainShader;
+}
