605780f9b9135834c4d3480ed2275d9594b333a3
andorapetcu <andorapetcu@yahoo.com>
2023-12-06 19:52:20 +0200

    Added camera to main function

--- Karting Simulator/Karting Simulator/main.cpp
+++ Karting Simulator/Karting Simulator/main.cpp
@@ -3,25 +3,30 @@
 #include "Skybox.h"
 #include "Camera.h"
 
-// Function to handle window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
 int main() {
-    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // Set GLFW to not create an OpenGL context (we'll use GLEW)
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-    // Create a GLFW windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(800, 600, "Skybox Example", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    Camera camera(800, 600, glm::vec3(0.0f, 0.0f, 0.0f));
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
         glfwTerminate();
         return -1;
     }
