8f0f7b751dbb586e9e7350a37b772b2ec3c85be9
Deniisaa <iliedenisaa@gmail.com>
2023-12-16 16:21:29 +0200

    Added glTexParameters

--- Karting Simulator/Karting Simulator/Skybox.cpp
+++ Karting Simulator/Karting Simulator/Skybox.cpp
@@ -124,4 +124,11 @@ unsigned int Skybox::LoadSkybox(const std::vector<std::string>& faces) {
             stbi_image_free(data);
         }
     }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
 }
