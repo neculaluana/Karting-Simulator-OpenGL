2d723ce6a882af8d573f5540ca81a4a7edea3d8e
Alex Popa <alexpopa1910@gmil.com>
2023-12-20 20:56:34 +0200

    Updated camera class

--- Karting Simulator/Karting Simulator/Camera.cpp
+++ Karting Simulator/Karting Simulator/Camera.cpp
@@ -1,83 +1,9 @@
 #include "Camera.h"
 
-void Camera::ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch)
-{
-	yaw += xOffset;
-	pitch += yOffset;
-	if (constrainPitch) {
-		if (pitch > 89.0f)
-			pitch = 89.0f;
-		if (pitch < -89.0f)
-			pitch = -89.0f;
-	}
-	UpdateCameraVectors();
+Camera::Camera(glm::vec3 position) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), Up(glm::vec3(0.0f, 1.0f, 0.0f)) {
+    Position = position;
 }
 
-void Camera::UpdateCameraVectors()
-{
-	this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	this->forward.y = sin(glm::radians(pitch));
-	this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	this->forward = glm::normalize(this->forward);
-	right = glm::normalize(glm::cross(forward, worldUp));
-	up = glm::normalize(glm::cross(right, forward));
-}
-
-Camera::Camera(const int width, const int height, const glm::vec3& position):
-	width(width), height(height), startPosition(position)
-{
-	Set(width, height, position);
-}
-
-void Camera::Set(const int width, const int height, const glm::vec3& position)
-{
-	this->width = width;
-	this->height = height; 
-	this->position = position;
-	isPerspective = true;
-	this->yaw = YAW;
-	this->pitch = PITCH;
-	this->zNear = zNEAR;
-	this->zFar = zFAR;
-	this->FoVy = FOV;
-
-	lastX = width / 2.0f;
-	lastY = height / 2.0f;
-
-	// Set the camera to look towards the negative Z-axis
-	this->forward = glm::vec3(0.0f, 0.0f, -1.0f);
-	this->up = glm::vec3(0, 1, 0);
-	this->right = glm::vec3(1.0f, 0.0f, 0.0f);
-	this->worldUp = up;
-
-	// Update the view and projection matrices
-	UpdateCameraVectors();
-}
-
-glm::mat4 Camera::GetViewMatrix() const {
-	return glm::lookAt(position, position + forward, up);
-}
-
-glm::mat4 Camera::GetSkyboxViewMatrix() const {
-	glm::mat4 view = glm::lookAt(glm::vec3(0.0f), forward, up);
-	return glm::mat4(glm::mat3(view)); // Remove translation for the skybox
-}
-
-
-// Function to get the projection matrix
-glm::mat4 Camera::GetProjectionMatrix() const {
-	glm::mat4 Proj = glm::mat4(1);
-	if (isPerspective)
-	{
-		float aspectRatio = ((float)(width)) / height;
-		Proj = glm::perspective(glm::radians(FoVy), aspectRatio, zNear, zFar);
-	}
-	else
-	{
-		float scaleFactor = 2000.f;
-		Proj = glm::ortho<float>(
-			-width / scaleFactor, width / scaleFactor,
-			-height / scaleFactor, height / scaleFactor, -zFar, zFar);
-	}
-	return Proj;
+glm::mat4 Camera::GetViewMatrix() {
+    return glm::lookAt(Position, Position + Front, Up);
 }
--- Karting Simulator/Karting Simulator/Camera.h
+++ Karting Simulator/Karting Simulator/Camera.h
@@ -1,75 +1,14 @@
-﻿#pragma once
-#include <stdlib.h> 
-#include <stdio.h>
-#include <math.h> 
+﻿#ifndef CAMERA_H
+#define CAMERA_H
 
-#include <GL/glew.h>
-
-#include <GLM.hpp>
+#include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
-
-#include <glfw3.h>
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-#pragma comment (lib, "glfw3dll.lib")
-#pragma comment (lib, "glew32.lib")
-#pragma comment (lib, "OpenGL32.lib")
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-enum class EDirection {
-	Unknown,
-	Forward,
-	Backward,
-	Left,
-	Right,
-	Up,
-	Down,
-};
 
 class Camera {
-private:
-	const float zNEAR = 0.1f;
-	const float zFAR = 500.f;
-	const float YAW = -90.0f;
-	const float PITCH = 0.0f;
-	const float FOV = 45.0f;
-	glm::vec3 startPosition;
-
-	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true);
-	void UpdateCameraVectors();
-protected:
-	const float cameraSpeedFactor = 50.0f;
-	const float mouseSensitivity = 0.25f;
-
-	float zNear;
-	float zFar;
-	float FoVy;
-	int width;
-	int height;
-	bool isPerspective;
-
-	glm::vec3 position;
-	glm::vec3 forward;
-	glm::vec3 right;
-	glm::vec3 up;
-	glm::vec3 worldUp;
-
-	float yaw;
-	float pitch;
-
-	bool bFirstMouseMove = true;
-	float lastX = 0.f, lastY = 0.f;
 public:
-	Camera(const int width, const int height, const glm::vec3& position);
-	void Set(const int width, const int height, const glm::vec3& position);
-	glm::mat4 GetViewMatrix() const;
-	glm::mat4 GetProjectionMatrix() const;
-	glm::mat4 GetSkyboxViewMatrix() const;
-
+    glm::vec3 Position, Front, Up;
+    Camera(glm::vec3 position);
+    glm::mat4 GetViewMatrix();
 };
+
+#endif
