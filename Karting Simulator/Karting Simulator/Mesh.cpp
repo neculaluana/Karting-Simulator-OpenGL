f4cfcc1d522345382a15d08093a42fbf5730987b
Deniisaa <iliedenisaa@gmail.com>
2023-12-27 23:09:49 +0200

    Added buffers in setupMesh

--- Karting Simulator/Karting Simulator/Mesh.cpp
+++ Karting Simulator/Karting Simulator/Mesh.cpp
@@ -61,4 +61,10 @@ void Mesh::Draw
 void Mesh::setupMesh() {
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 }
