9b27be807e9775f980d11835933fbe131003c4fa
Deniisaa <iliedenisaa@gmail.com>
2023-12-27 23:10:47 +0200

    vertex positions in setupMesh

--- Karting Simulator/Karting Simulator/Mesh.cpp
+++ Karting Simulator/Karting Simulator/Mesh.cpp
@@ -67,4 +67,10 @@ void Mesh::setupMesh() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+	// Vertex Positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 }
