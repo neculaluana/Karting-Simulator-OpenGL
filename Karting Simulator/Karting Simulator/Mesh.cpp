7a68be97afe19ab7959c365e985cb1557e2e5614
Deniisaa <iliedenisaa@gmail.com>
2023-12-27 23:08:43 +0200

    Created setupMesh

--- Karting Simulator/Karting Simulator/Mesh.cpp
+++ Karting Simulator/Karting Simulator/Mesh.cpp
@@ -1,21 +1,10 @@
 #include "Mesh.h"
 
-Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures)
+Mesh::Mesh(std::vector <Vertex>& vertices, std::vector <GLuint>& indices, std::vector <Texture>& textures) :vertices(vertices),
+indices(indices),
+textures(textures)
 {
-	Mesh::vertices = vertices;
-	Mesh::indices = indices;
-	Mesh::textures = textures;
-
-	VAO.Bind();
-	VBO VBO(vertices);
-	EBO EBO(indices);
-	VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, sizeof(Vertex), (void*)0);
-	VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, sizeof(Vertex), (void*)(3 * sizeof(float)));
-	VAO.LinkAttrib(VBO, 2, 3, GL_FLOAT, sizeof(Vertex), (void*)(6 * sizeof(float)));
-	VAO.LinkAttrib(VBO, 3, 2, GL_FLOAT, sizeof(Vertex), (void*)(9 * sizeof(float)));
-	VAO.Unbind();
-	VBO.Unbind();
-	EBO.Unbind();
+	setupMesh();
 }
 
 
@@ -67,4 +56,9 @@ void Mesh::Draw
 	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(matrix));
 
 	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+}
+
+void Mesh::setupMesh() {
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
 }
